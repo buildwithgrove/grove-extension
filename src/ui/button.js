91336d1e6d647b1cbeaee9a6e11eda98e6110145
Daniel Olshansky <olshansky.daniel@gmail.com>
2025-10-31 16:13:25 -0700

    Looking good

--- src/ui/button.js
+++ src/ui/button.js
@@ -36,27 +36,12 @@ class TipButton {
     innerDiv.className = 'css-146c3p1 r-bcqeeo r-qvutc0 r-37j5jr r-q4m81j r-a023e6 r-rjixqe r-b88u0q r-1awozwy r-6koalj r-18u37iz r-16y2uox r-1777fci';
     innerDiv.style.color = 'rgb(239, 243, 244)';
 
-    // Create SVG icon (using money/tip icon)
-    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-    svg.setAttribute('viewBox', '0 0 24 24');
-    svg.setAttribute('aria-hidden', 'true');
-    svg.setAttribute('class', 'r-4qtqp9 r-yyyyoo r-dnmrzs r-bnwqim r-lrvibr r-m6rgpd r-z80fyv r-19wmn03');
-    svg.style.color = 'rgb(239, 243, 244)';
-
-    const g = document.createElementNS('http://www.w3.org/2000/svg', 'g');
-    const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-    path.setAttribute('d', 'M23 3v14h-2V5H5V3h18zM10 17c1.1 0 2-1.34 2-3s-.9-3-2-3-2 1.34-2 3 .9 3 2 3zM1 7h18v14H1V7zm16 10c-1.1 0-2 .9-2 2h2v-2zm-2-8c0 1.1.9 2 2 2V9h-2zM3 11c1.1 0 2-.9 2-2H3v2zm0 4c2.21 0 4 1.79 4 4h6c0-2.21 1.79-4 4-4v-2c-2.21 0-4-1.79-4-4H7c0 2.21-1.79 4-4 4v2zm0 4h2c0-1.1-.9-2-2-2v2z');
-
-    g.appendChild(path);
-    svg.appendChild(g);
-
-    // Create text span
+    // Create text span (no SVG icon, just text)
     const textSpan = document.createElement('span');
     textSpan.className = 'css-1jxf684 r-dnmrzs r-1udh08x r-1udbk01 r-3s2u2q r-bcqeeo r-1ttztb7 r-qvutc0 r-poiln3 r-a023e6 r-rjixqe';
     textSpan.textContent = 'Tip 🌿';
 
     // Assemble the structure
-    innerDiv.appendChild(svg);
     innerDiv.appendChild(textSpan);
     this.button.appendChild(innerDiv);
 
