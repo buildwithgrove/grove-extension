f3136e45a176661f3edae590a3faa082eeebb6b7
Arthur Ariel Sabintsev <arthur@sabintsev.com>
2025-12-18 23:20:29 -0500

    chore: bump version to 1.0.8

--- makefiles/build.mk
+++ makefiles/build.mk
@@ -4,7 +4,7 @@
 
 # Extension metadata
 EXTENSION_NAME := grove-extension
-VERSION := 1.0.7.7
+VERSION := 1.0.8
 GIT_SHA := $(shell git rev-parse --short HEAD)
 VERSION_FULL := $(VERSION)-$(GIT_SHA)
 
